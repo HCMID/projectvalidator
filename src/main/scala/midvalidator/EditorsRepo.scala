e0116360dc2f4abe19802fe1cdf89df1ca5875a2
Neel Smith <dnsmith.neel@gmail.com>
2018-09-05 14:07:23 -0400

    Flesh out API docs

--- src/main/scala/midvalidator/EditorsRepo.scala
+++ src/main/scala/midvalidator/EditorsRepo.scala
@@ -12,18 +12,29 @@ import better.files.Dsl._
 * @param baseDir Root directory of repository.
 */
 case class EditorsRepo(baseDir: String)  {
+  /** Directory for DSE records (in CEX format).*/
   val dseDir = File(baseDir + "/dse")
+  /** Writable directory for validation reports. */
   val validationDir = File(baseDir + "/validation")
+  /** Directory with cataloged editions of texts. */
   val editionsDir = File(baseDir + "/editions")
+  /** Directory with paleographic observations (in CEX format).*/
   val paleographyDir = File(baseDir + "/paleography")
+  /** Directory with library headers for building composite CEX file.*/
   val libHeadersDir = File(baseDir + "/header")
+  /** Vector of all required directories for an HCMID editorial project.*/
   val dirs = Vector(dseDir, editionsDir, validationDir, paleographyDir, libHeadersDir)
 
   for (d <- dirs) {
     require(d.exists, "Repository not correctly laid out: missing directory " + d)
   }
+
+  
+  /** Catalog of edited texts.*/
   val ctsCatalog = editionsDir/"catalog.cex"
+  /** Configuration of citation for local files (in any supported format).*/
   val ctsCitation = editionsDir/"citation.cex"
+
   for (conf <- Seq(ctsCatalog, ctsCitation)) {
     require(conf.exists,"Missing required configuration file: " + conf)
   }
