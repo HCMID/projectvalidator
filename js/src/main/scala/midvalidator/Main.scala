c538e0a94a250f1ae9fd27930b51f3c58aa4b394
Neel Smith <dnsmith.neel@gmail.com>
2019-02-09 11:16:08 -0500

    Set up main to test access from JS branch

---
+++ js/src/main/scala/midvalidator/Main.scala
@@ -0,0 +1,11 @@
+package edu.holycross.shot.mid.validator
+
+import scala.scalajs.js
+
+object Main extends js.JSApp {
+  def main(): Unit = {
+
+    println("Testing readers and orthography in a main object in JS branch:")
+
+  }
+}
