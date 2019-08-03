28df858905def31dbf6cd66c4f7584a2862506d3
Neel Smith <dnsmith.neel@gmail.com>
2019-08-03 12:26:32 -0400

    Rm deprecated class

---
+++ shared/src/test/scala/midvalidator/DseResultsSpec.scala
@@ -0,0 +1,13 @@
+package edu.holycross.shot.mid.validator
+import edu.holycross.shot.ohco2._
+import edu.holycross.shot.cite._
+import org.scalatest.FlatSpec
+
+
+class DseResultsSpec extends FlatSpec {
+
+  "A DseResults" should "determine if a DsePassage reports a node in the given corpus" in pending
+  it should "return a TestReport" in pending
+
+
+}
