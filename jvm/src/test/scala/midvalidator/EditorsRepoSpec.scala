23ea6e16f2baa08fc101cc0adb41008bbb1254f8
Neel Smith <dnsmith.neel@gmail.com>
2019-08-03 14:14:55 -0400

    Clean up some unit tests

--- jvm/src/test/scala/midvalidator/EditorsRepoSpec.scala
+++ jvm/src/test/scala/midvalidator/EditorsRepoSpec.scala
@@ -9,6 +9,12 @@ import edu.holycross.shot.cite._
 */
 class EditorsRepoSpec extends FlatSpec {
 
-  "An EditorsRepo" should "find a diplomatic MarkupReader for a given CTS URN" in pending 
+  "An EditorsRepo" should "build a CITE Library from source files" in pending
+  it should "build orthographies from souce files" in pending
+  it should "build a Vector of ReaderPairings from source files" in pending
+  it should "find a diplomatic MarkupReader for a given CTS URN" in pending
+  it should "construct a DseVector for the records in this repository" in pending
+  it should "constuct a TextRepository for the records in this repository" in pending
+
 
 }
--- jvm/src/test/scala/midvalidator/RepoConfigSpec.scala
+++ jvm/src/test/scala/midvalidator/RepoConfigSpec.scala
@@ -9,62 +9,70 @@ import edu.holycross.shot.cite._
 */
 class RepoConfigSpec extends FlatSpec {
 
-  "An EditorsRepo" should "fail if missing dse dir" in pending /*{
+
+  val readerMap : Map[String, Vector[MidMarkupReader]] = Map(
+    "MidProseABDiplomatic" ->   Vector(MidProseABDiplomatic)
+  )
+  val orthoMap : Map[String, MidOrthography] = Map(
+    "Latin23" -> Latin23
+  )
+
+  "An EditorsRepo" should "fail if missing dse dir" in {
     val badSrc = "jvm/src/test/resources/nodse"
     try {
-      val repo = EditorsRepo(badSrc)
+      val repo = EditorsRepo(badSrc, readerMap, orthoMap)
       fail("Should not have created repo.")
     } catch {
       case err: Throwable => assert(true)
     }
-  }*/
-  it  should "fail if missing editing dir" in pending /*{
+  }
+  it  should "fail if missing editing dir" in {
     val badSrc = "jvm/src/test/resources/noeditions"
     try {
-      val repo = EditorsRepo(badSrc)
+      val repo = EditorsRepo(badSrc, readerMap, orthoMap)
       fail("Should not have created repo.")
     } catch {
       case err: Throwable => assert(true)
     }
-  }*/
-  it  should "fail if missing validation dir" in pending /*{
+  }
+  it  should "fail if missing validation dir" in {
     val badSrc = "jvm/src/test/resources/novalidation"
     try {
-      val repo = EditorsRepo(badSrc)
+      val repo = EditorsRepo(badSrc, readerMap, orthoMap)
       fail("Should not have created repo.")
     } catch {
       case err: Throwable => assert(true)
     }
-  }*/
-  it  should "fail if missing paleography dir" in pending /* {
+  }
+  it  should "fail if missing paleography dir" in  {
     val badSrc = "jvm/src/test/resources/nopaleography"
     try {
-      val repo = EditorsRepo(badSrc)
+      val repo = EditorsRepo(badSrc, readerMap, orthoMap)
       fail("Should not have created repo.")
     } catch {
       case err: Throwable => assert(true)
     }
   }
-*/
-  it  should "fail if missing CTS catalog" in pending /*{
+
+  it  should "fail if missing CTS catalog" in {
     val badSrc = "jvm/src/test/resources/noctscatalog"
     try {
-      val repo = EditorsRepo(badSrc)
-      fail("Should not have created repo.")
-    } catch {
-      case err: Throwable => assert(true)
-    }
-  }*/
-
-  it  should "fail if missing CTS citation configuration" in pending /*{
-    val badSrc = "jvm/src/test/resources/noctscitation"
-    try {
-      val repo = EditorsRepo(badSrc)
+      val repo = EditorsRepo(badSrc, readerMap, orthoMap)
       fail("Should not have created repo.")
     } catch {
       case err: Throwable => assert(true)
     }
   }
-*/
+
+  it  should "fail if missing CTS citation configuration" in {
+    val badSrc = "jvm/src/test/resources/noctscitation"
+    try {
+      val repo = EditorsRepo(badSrc, readerMap, orthoMap)
+      fail("Should not have created repo.")
+    } catch {
+      case err: Throwable => assert(true)
+    }
+  }
+
 
 }
