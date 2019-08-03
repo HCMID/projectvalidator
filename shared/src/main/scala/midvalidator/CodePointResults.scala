58c3b1d46c7ad047360bbdb9a17b785adfb39b66
Neel Smith <dnsmith.neel@gmail.com>
2019-08-03 16:02:29 -0400

    Implement TestResults for CodePoint validation

---
+++ shared/src/main/scala/midvalidator/CodePointResults.scala
@@ -0,0 +1,44 @@
+package edu.holycross.shot.mid.validator
+import scala.scalajs.js.annotation._
+
+import edu.holycross.shot.cite._
+
+/** Implementation of [[TestResults]] for Strings of characters
+* in a given orthographic system.
+*
+* @param ortho MidOrthography.
+*/
+@JSExportAll case class CodePointResults[T](ortho: MidOrthography) extends TestResults[T] {
+
+  /**
+  */
+  def report(str: T):  TestReport = {
+    good(str) match {
+      case true =>    {
+        str match {
+          case s: String => {
+
+            TestReport(true, "maybe true? " + s)
+          }
+        }
+
+      }
+      case false => TestReport(false,"This value is not a String: " + str)
+    }
+  }
+
+  /**
+  */
+  def good(str: T): Boolean = {
+    str match {
+      case s: String =>  {
+        ortho.validString(s)
+      }
+
+      case _ => {
+        println(s"${str}  was NOT a string")
+        false
+      }
+    }
+  }
+}
