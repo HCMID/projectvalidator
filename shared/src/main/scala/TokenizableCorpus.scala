f7e7d1f99a1eb3b1ee0c4185915552410af8d387
Neel Smith <dnsmith.neel@gmail.com>
2019-08-20 14:19:55 -0400

    Test TokenizableCorpus

--- shared/src/main/scala/TokenizableCorpus.scala
+++ shared/src/main/scala/TokenizableCorpus.scala
@@ -8,9 +8,24 @@ import edu.holycross.shot.histoutils._
 */
 @JSExportAll case class TokenizableCorpus(corpus: Corpus, orthography: MidOrthography) {
 
-  def tokenizedCorpus = orthography.tokenizedCorpus(corpus)
+  /** Tokenize OHCO2 Corpus in given orthography.*/
+  lazy val tokenizedCorpus :  Corpus = {
+    orthography.tokenizedCorpus(corpus)
+  }
 
-  def wordList : Vector[String] = Vector.empty[String]
+  lazy val tokens :  Vector[MidToken] = {
+    corpus.nodes.flatMap(orthography.tokenizeNode(_))
+  }
+
+  /** Filter out lexical tokens.*/
+  lazy val lexicalTokens  :  Vector[MidToken]  = {
+    tokens.filter(_.tokenCategory.toString == "Some(LexicalToken)")
+  }
+
+  /** Create alphabetical word list for corpus.*/
+  lazy val wordList : Vector[String] = {
+    lexicalTokens.map(_.string.toLowerCase).distinct.sorted
+  }
 
   def lexHistogram: Histogram[String] = Histogram(Vector.empty[Frequency[String]])
 
