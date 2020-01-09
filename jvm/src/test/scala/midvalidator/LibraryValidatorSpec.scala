3a6fe0a5f8266340862f42a304391f6d8d9b6d80
Neel Smith <dnsmith.neel@gmail.com>
2020-01-09 17:32:27 -0500

    Add test on LibraryValidator

---
+++ jvm/src/test/scala/midvalidator/LibraryValidatorSpec.scala
@@ -0,0 +1,28 @@
+package edu.holycross.shot.mid.validator
+import edu.holycross.shot.ohco2._
+import edu.holycross.shot.cite._
+import edu.holycross.shot.dse._
+
+import org.scalatest.FlatSpec
+
+
+class LibraryValidatorSpec extends FlatSpec {
+
+  // Build a CiteLibrary from an EditorsRepo for test data
+  val readerMap = Map.empty[String, Vector[MidMarkupReader]]
+  val orthoMap = Map.empty[String, MidOrthography]
+  val repo = EditorsRepo("jvm/src/test/resources/chantsample",
+  readerMap, orthoMap)
+  val lib = repo.library
+
+  "A LibraryValidator" should "apply a list of validators of any type" in {
+    val dsev = DseValidator(lib)
+    val validatorList = Vector(dsev)
+    val libval = LibraryValidator(lib)
+    val pg =   Cite2Urn("urn:cite2:ecod:sg359pages.v1:36")
+    val rslts = libval.validate(pg, validatorList)
+    println("Library validator: " + rslts.size + " tests run.")
+  }
+
+
+}
